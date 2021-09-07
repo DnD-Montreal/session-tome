49497a4a9037daba00ce48698369302ecbb27562
Wilson <wilsonhuang00815@gmail.com>
2021-09-07 15:51:23 -0400

    chore: #9 configure front end linter

--- resources/js/Components/Checkbox.js
+++ resources/js/Components/Checkbox.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-export default function Checkbox({ name, value, handleChange }) {
+export default function Checkbox({name, value, handleChange}) {
     return (
         <input
-            type="checkbox"
+            type='checkbox'
             name={name}
             value={value}
-            className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            className='rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             onChange={(e) => handleChange(e)}
         />
-    );
+    )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
     resolve: {
-        alias: {
-            '@': path.resolve('resources/js'),
-        },
+        plugins: [new TsconfigPathsPlugin({})],
     },
-};
+}
