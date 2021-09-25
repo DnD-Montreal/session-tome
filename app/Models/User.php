061374f91a5615d80cc65b3f8c934afeef42e37e
Massimo Triassi <kidmax14@hotmail.com>
2021-09-25 16:19:03 -0400

    fix: #60 extend correct base class for user
    add missing traits

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
