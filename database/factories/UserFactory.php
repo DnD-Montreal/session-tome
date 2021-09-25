25ec7ddbd783a4d6c2506ca4a43a270d4f45183e
Massimo Triassi <kidmax14@hotmail.com>
2021-09-25 16:19:53 -0400

    chore: #60 change user factory to use standarized password
    remove un-needed fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'email' => $this->faker->safeEmail(),
-            'email_verified_at' => $this->faker->dateTime(),
-            'password' => $this->faker->password(),
-            'remember_token' => $this->faker->word(),
-            'created_at' => $this->faker->dateTime(),
-            'updated_at' => $this->faker->dateTime(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
