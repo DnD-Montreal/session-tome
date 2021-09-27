95cf825cc7608731d1ba538c022170ccb43c0cdf
jason gadoury <jason_gadoury@hotmail.com>
2021-09-26 22:14:20 -0400

    test: #60 write unit tests for League model

--- tests/Unit/Models/LeagueTest.php
+++ tests/Unit/Models/LeagueTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Event;
+use App\Models\League;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use JMac\Testing\Traits\AdditionalAssertions;
@@ -14,12 +17,25 @@ class LeagueTest extends TestCase
     use WithFaker;
 
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function can_belong_to_users()
     {
-        $this->assertTrue(true);
+        $league = League::factory(1)->create()[0];
+        $user = User::factory(1)->create();
+        $league->users()->attach($user);
+
+        $this->assertCount(1, $league->users()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function can_have_events()
+    {
+        Event::factory(1)->create();
+        $league = League::first();
+
+        $this->assertCount(1, $league->events()->get());
     }
 }
