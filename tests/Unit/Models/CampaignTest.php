9648e5ad931f8f2e50f8a2c08948e70652010a59
jason gadoury <jason_gadoury@hotmail.com>
2021-09-26 20:10:26 -0400

    test: #60 write tests for Campaign model

--- tests/Unit/Models/CampaignTest.php
+++ tests/Unit/Models/CampaignTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Campaign;
+use App\Models\Character;
+use App\Models\Entry;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use JMac\Testing\Traits\AdditionalAssertions;
@@ -13,13 +17,55 @@ class CampaignTest extends TestCase
     use RefreshDatabase;
     use WithFaker;
 
+
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function can_belong_to_adventure()
     {
-        $this->assertTrue(true);
+        $campaign = Campaign::factory(1)->create()[0];
+
+        $this->assertcount(1, $campaign->adventure()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function can_belong_to_characters()
+    {
+        ## Assign
+        $character = Character::factory(3)->create();
+        $campaign = Campaign::factory(1)->create()[0];
+
+        # Act
+        $campaign->characters()->attach($character);
+
+        # Assert
+        $this->assertCount(3, $campaign->characters);
+    }
+
+    /**
+     * @test
+     */
+    public function can_belong_to_users()
+    {
+        //User::factory(1)->create();
+        $user = User::factory(2)->create();
+        $campaign = Campaign::factory(1)->create()[0];
+
+        $campaign->users()->attach($user);
+
+        $this->assertcount(2, $campaign->users);
+    }
+
+    /**
+     * @test
+     */
+    public function can_have_entries()
+    {
+        Entry::factory(1)->create();
+        $campaign = Campaign::first();
+
+        $this->assertcount(1, $campaign->entries()->get());
     }
 }
