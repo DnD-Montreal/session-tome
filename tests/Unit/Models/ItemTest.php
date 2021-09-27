fe280d0fb91a3c752f76f45e8e9e68427cae50ea
jason gadoury <jason_gadoury@hotmail.com>
2021-09-26 22:13:02 -0400

    test: #60 write unit tests for Item model

--- tests/Unit/Models/ItemTest.php
+++ tests/Unit/Models/ItemTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Entry;
+use App\Models\Item;
+use App\Models\Trade;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use JMac\Testing\Traits\AdditionalAssertions;
@@ -14,12 +17,35 @@ class ItemTest extends TestCase
     use WithFaker;
 
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function can_belong_to_trades()
     {
-        $this->assertTrue(true);
+        $trade = Trade::factory(1)->create();
+        $item = Item::factory(1)->create()[0];
+
+        $item->trades()->attach($trade);
+
+        $this->assertCount(1, $item->trades()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function can_belong_to_entry()
+    {
+        $item = Item::factory(1)->create()[0];
+
+        $this->assertCount(1, $item->entry()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function can_belong_to_character()
+    {
+        $item = Item::factory(1)->create()[0];
+
+        $this->assertCount(1, $item->character()->get());
     }
 }
