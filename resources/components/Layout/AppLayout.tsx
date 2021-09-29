e72c592e1da6af712c16bec27fb354a5506aa2b3
luigibesani <luigi.besani@gmail.com>
2021-09-29 01:58:38 -0400

    fix: #49 replaced href values from '#' to '/#'

--- resources/components/Layout/AppLayout.tsx
+++ resources/components/Layout/AppLayout.tsx
@@ -8,51 +8,62 @@ import './AppLayout.css'
 
 // ToDo: implement app layout component
 const AppLayout = () => (
-  <Grid id='gridStyle' container>
-    <Grid
-      id='primaryRow'
-      item
-      container
-      justifyContent='space-between'
-      alignItems='center'>
-      <Grid item xs={4}>
-        <SVG src={associationLogo} width={130} height={60} />
-      </Grid>
-      <Grid item xs={4}>
-        <SVG src={applicationLogo} width={278} height={51} />
-      </Grid>
-      <Grid item xs={2}>
-        <Link href='#'>DragonSlayer999</Link>
-      </Grid>
-      <Grid item xs={2}>
-        <Avatar id='userAvatar' sx={{bgcolor: '#FF4500'}}>
-          DS
-        </Avatar>
-      </Grid>
+    <Grid id='gridStyle' container>
+        <Grid
+            id='primaryRow'
+            item
+            container
+            justifyContent='space-between'
+            alignItems='center'>
+            <Grid item xs={3}>
+                <SVG src={associationLogo} width={130} height={60} />
+            </Grid>
+            <Grid item xs={6}>
+                <SVG src={applicationLogo} width={278} height={51} />
+            </Grid>
+            <Grid item xs={2}>
+                <p>DragonSlayer999</p>
+            </Grid>
+            <Grid item xs={1}>
+                <Avatar id='userAvatar' sx={{bgcolor: '#FF4500'}}>
+                    DS
+                </Avatar>
+            </Grid>
+        </Grid>
+        <Grid
+            id='secondaryRow'
+            item
+            container
+            direction='row'
+            spacing={0}
+            justifyContent='center'>
+            <Grid item xs={2}>
+                <Link underline='none' color='white' href='/#'>
+                    Home
+                </Link>
+            </Grid>
+            <Grid item xs={2}>
+                <Link underline='none' color='white' href='/#'>
+                    Characters
+                </Link>
+            </Grid>
+            <Grid item xs={2}>
+                <Link underline='none' color='white' href='/#'>
+                    Item Shop
+                </Link>
+            </Grid>
+            <Grid item xs={2}>
+                <Link underline='none' color='white' href='/#'>
+                    Campaigns
+                </Link>
+            </Grid>
+            <Grid item xs={2}>
+                <Link underline='none' color='white' href='/#'>
+                    Ratings
+                </Link>
+            </Grid>
+        </Grid>
     </Grid>
-    <Grid
-      id='secondaryRow'
-      item
-      container
-      direction='row'
-      justifyContent='center'>
-      <Grid item xs>
-        <Link href='#'>Home</Link>
-      </Grid>
-      <Grid item xs>
-        <Link href='#'>Characters</Link>
-      </Grid>
-      <Grid item xs>
-        <Link href='#'>Item Shop</Link>
-      </Grid>
-      <Grid item xs>
-        <Link href='#'>Campaigns</Link>
-      </Grid>
-      <Grid item xs>
-        <Link href='#'>Ratings</Link>
-      </Grid>
-    </Grid>
-  </Grid>
 )
 
 AppLayout.displayName = 'AppLayout'
