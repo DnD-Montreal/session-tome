81e21a426e2419eb4858d04a26fc7ce9b149ecd1
luigibesani <luigi.besani@gmail.com>
2021-09-29 00:59:22 -0400

    chore: #49 add app layout skeleton + configuration

--- resources/app.tsx
+++ resources/app.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import {render} from 'react-dom'
 import {InertiaApp} from '@inertiajs/inertia-react'
 import {InertiaProgress} from '@inertiajs/progress'
+import AppLayout from './components/Layout/AppLayout'
 
 InertiaProgress.init({
-    color: '#ED8936',
-    showSpinner: true,
+  color: '#ED8936',
+  showSpinner: true,
 })
 
 const app = document.getElementById('app')
 
 render(
-    <>
-        <InertiaApp
-            initialPage=''
-            resolveComponent={(name) =>
-                import(`./Pages/${name}`).then((module) => module.default)
-            }
-        />
-        <p>Test</p>
-    </>,
-    app,
+  <>
+    <InertiaApp
+      initialPage=''
+      resolveComponent={(name) =>
+        import(`./Pages/${name}`).then((module) => module.default)
+      }
+    />
+    <AppLayout />
+  </>,
+  app,
 )
--- resources/components/Layout/AppLayout.tsx
+++ resources/components/Layout/AppLayout.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import Grid from '@mui/material/Grid'
+import SVG from 'react-inlinesvg'
+import {Avatar, Link} from '@mui/material'
+import associationLogo from '../../icons/ParentAssociationLogoWhite.svg'
+import applicationLogo from '../../icons/SessionTomeOfficialLogo.svg'
+import './AppLayout.css'
 
 // ToDo: implement app layout component
-const AppLayout = () => <div />
+const AppLayout = () => (
+  <Grid id='gridStyle' container>
+    <Grid
+      id='primaryRow'
+      item
+      container
+      justifyContent='space-between'
+      alignItems='center'>
+      <Grid item xs={4}>
+        <SVG src={associationLogo} width={130} height={60} />
+      </Grid>
+      <Grid item xs={4}>
+        <SVG src={applicationLogo} width={278} height={51} />
+      </Grid>
+      <Grid item xs={2}>
+        <Link href='#'>DragonSlayer999</Link>
+      </Grid>
+      <Grid item xs={2}>
+        <Avatar id='userAvatar' sx={{bgcolor: '#FF4500'}}>
+          DS
+        </Avatar>
+      </Grid>
+    </Grid>
+    <Grid
+      id='secondaryRow'
+      item
+      container
+      direction='row'
+      justifyContent='center'>
+      <Grid item xs>
+        <Link href='#'>Home</Link>
+      </Grid>
+      <Grid item xs>
+        <Link href='#'>Characters</Link>
+      </Grid>
+      <Grid item xs>
+        <Link href='#'>Item Shop</Link>
+      </Grid>
+      <Grid item xs>
+        <Link href='#'>Campaigns</Link>
+      </Grid>
+      <Grid item xs>
+        <Link href='#'>Ratings</Link>
+      </Grid>
+    </Grid>
+  </Grid>
+)
 
 AppLayout.displayName = 'AppLayout'
 export default AppLayout
