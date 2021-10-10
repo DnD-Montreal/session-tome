fb39a4f3a06d8f1a30253802e56b062ff5f2fd65
wilson <wilsonhuang00815@gmail.com>
2021-10-10 19:21:38 -0400

    chore: #35 update

--- resources/js/Pages/Test/Test.tsx
+++ resources/js/Pages/Test/Test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
-import route from 'ziggy-js'
 import {ApplicationLayout} from '../../Layouts'
 
 // Individual page component example
@@ -11,7 +10,6 @@ const Test = () => (
         <h1>Test Component</h1>
         <p>This is a test component.</p>
         <Button variant='contained'>Test Button</Button>
-        <p>here is a route: {route('user.index')}</p>
         <p>Here is a text field:</p>
         <TextField id='filled-basic' label='Filled' variant='filled' />
     </div>
