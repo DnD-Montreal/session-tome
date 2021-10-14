be3a7a0716a484d5d4c94eb6fa7f26509df0ef86
Massimo Triassi <kidmax14@hotmail.com>
2021-10-14 00:55:17 -0400

    test: #83 update tests to set up logged in user + add acting as for modification routes

--- tests/Feature/Http/Controllers/CampaignControllerTest.php
+++ tests/Feature/Http/Controllers/CampaignControllerTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Adventure;
 use App\Models\Campaign;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use JMac\Testing\Traits\AdditionalAssertions;
 use Tests\TestCase;
 
@@ -17,6 +19,15 @@ class CampaignControllerTest extends TestCase
     use AdditionalAssertions;
     use RefreshDatabase;
     use WithFaker;
+
+    public $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        Auth::login($this->user);
+    }
 
     /**
      * @test
