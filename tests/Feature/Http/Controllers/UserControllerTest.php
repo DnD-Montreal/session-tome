d9b90087520398ccc219a65f3ddc239a4202e822
Massimo Triassi <kidmax14@hotmail.com>
2021-10-14 11:45:15 -0400

    test: #83 add login set up step to allow tests past auth middleware

--- tests/Feature/Http/Controllers/UserControllerTest.php
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use JMac\Testing\Traits\AdditionalAssertions;
 use Tests\TestCase;
 
@@ -16,6 +17,15 @@ class UserControllerTest extends TestCase
     use AdditionalAssertions;
     use RefreshDatabase;
     use WithFaker;
+
+    public $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        Auth::login($this->user);
+    }
 
     /**
      * @test
