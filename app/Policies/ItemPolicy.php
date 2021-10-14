3c2f7306bec1bc15e29a9f5a6a51977ff6449e5c
Pascal <pascal.demerdjian@gmail.com>
2021-10-13 22:58:26 -0400

    feat: #65 create update policy for item

---
+++ app/Policies/ItemPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Item;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ItemPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user, Item $item)
+    {
+        return $item->user()->id == $user->id || $user->isSiteAdmin();
+    }
+}
