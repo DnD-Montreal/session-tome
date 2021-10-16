d2638db2f7a0613e00727ef442bdc2f9f2af7c0a
bcobo341 <beatrice.cobo@icloud.com>
2021-10-16 16:41:22 -0400

    test: #56 add test for /Character

--- resources/js/Pages/Character/Character.test.js
+++ resources/js/Pages/Character/Character.test.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {render, screen, fireEvent} from '@testing-library/react'
 import Character from './Character'
 
-// enzyme unit test example
 describe('Character', () => {
     it('Component should render', () => {
         const component = shallow(<Character />)
         expect(component).toBeDefined()
+    })
+
+    it('should go to correct steps', () => {
+        render(<Character />)
+        fireEvent.click(screen.getByText('Create'))
+        expect(screen.getByRole('link')).toHaveAttribute('href', '/')
+        fireEvent.click(screen.getByText('Export'))
+        const searchInput = screen
+            .getByRole('label')
+            .getByText('Search Character')
+        fireEvent.change(searchInput, {target: {value: 'test'}})
+        expect(searchInput.value).toBe('test')
     })
 })
