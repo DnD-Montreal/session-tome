e914b3d1055cf89f81f141471b956e59e9c33bcd
bcobo341 <beatrice.cobo@icloud.com>
2021-10-17 22:12:55 -0400

    fix: #56 improve CharTable test coverage

--- resources/js/Pages/Character/CharTable.test.js
+++ resources/js/Pages/Character/CharTable.test.js
@@ -22,17 +22,17 @@ describe('CharTable', () => {
         render(<CharTable {...props} />)
         const checkbox = screen.getAllByRole('checkbox')
         expect(checkbox).toHaveLength(charData.length)
-        fireEvent.click(screen.getByTestId('header-checkbox'))
+        fireEvent.click(screen.getByLabelText('select all names'))
         for (let i = 0; i < 5; i += 1) {
             const checkboxel = screen
                 .getByTestId(`checkbox-${i}`)
                 .querySelector('input[type="checkbox"]')
-            expect(checkboxel).toHaveProperty('checked', false)
+            expect(checkboxel).toHaveProperty('checked', true)
         }
         const checkbox1 = screen
             .getByTestId('checkbox-1')
             .querySelector('input[type="checkbox"]')
         fireEvent.click(screen.getByTestId('checkbox-1'))
-        expect(checkbox1).toHaveProperty('checked', true)
+        expect(checkbox1).toHaveProperty('checked', false)
     })
 })
