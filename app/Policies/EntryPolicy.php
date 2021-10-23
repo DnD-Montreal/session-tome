a33e287cdd87b8f3b6d02a3ed31311e7383d8e2f
IanjhPhillips <ianjhphillips@gmail.com>
2021-10-23 17:53:15 -0400

    feat: #113 create EntryPolicy

---
+++ app/Policies/EntryPolicy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Character;
+use App\Models\User;
+use App\Models\Entry;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Http\Request;
+
+class EntryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Entry  $entry
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Entry $entry)
+    {
+        return $user->id == $entry->user_id || $user->isSiteAdmin();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Entry  $entry
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Entry $entry)
+    {
+        return $user->id == $entry->user_id || $user->isSiteAdmin();
+    }
+}
