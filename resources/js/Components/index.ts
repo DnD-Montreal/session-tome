f6a19831991a5385939a9e6bfe25b078db4637c4
luigibesani <luigi.besani@gmail.com>
2021-10-25 19:14:35 -0400

    chore: #122 add RarityChip export to components index.ts

--- resources/js/Components/index.ts
+++ resources/js/Components/index.ts
@@ -1,5 +1,6 @@
 export {default as Authentication} from './Authentication'
 export {default as FactionChip} from './FactionChip'
+export {default as RarityChip} from './RarityChip'
 
 // Tables
 export {default as ItemTable} from './Table/ItemTable'
