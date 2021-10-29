f95cd1e7210cb2de23875c77da2935b5f4bdc253
IanjhPhillips <ianjhphillips@gmail.com>
2021-10-29 15:36:05 -0400

    feat: #117 add DMEntry controller

---
+++ app/Http/Controllers/DMEntryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EntryStoreRequest;
+use App\Models\Entry;
+use Illuminate\Http\Request;
+
+class DMEntryController extends Controller
+{
+    public function index(Request $request)
+    {
+        $entries = DB::table('entries')
+            ->where('type', Entry::TYPE_DM)
+            ->get();
+
+        return view('entry.index', compact('entries'));
+    }
+}
