54a20d344027e3f5331b10824179f227eb96a129
IanjhPhillips <ianjhphillips@gmail.com>
2021-10-29 12:58:34 -0400

    chore: #142 add basic info annotation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,32 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    /**
+     * @OA\Info(
+     *      version="0.0.1",
+     *      title="Session Tome API Documentation",
+     *      description="Session Tome API description",
+     *      @OA\Contact(
+     *          email="massimo@dndmtl.com"
+     *      ),
+     *      @OA\License(
+     *          name="MIT License",
+     *          url="https://opensource.org/licenses/MIT"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description=""
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Session Tome",
+     *     description="API Endpoints of Session Tome"
+     * )
+     */
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 }
