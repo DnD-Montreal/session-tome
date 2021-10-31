99cac8ff6dd8ce8ee0819ddfc4d227e9cfe9fa4d
Massimo Triassi <kidmax14@hotmail.com>
2021-10-30 21:55:20 -0400

    feat: #100 add exception handler render func for intertia

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Inertia\Inertia;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,6 +28,20 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    protected $titles = [
+        503 => '503: Service Unavailable',
+        500 => '500: Server Error',
+        404 => '404: Page Not Found',
+        403 => '403: Forbidden',
+    ];
+
+    protected $descriptions = [
+        503 => 'Sorry, we are doing some maintenance. Please check back soon.',
+        500 => 'Whoops, something went wrong on our servers.',
+        404 => 'Sorry, the page you are looking for could not be found.',
+        403 => 'Sorry, you are forbidden from accessing this page.',
+    ];
+
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -37,5 +52,28 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Prepare exception for rendering.
+     *
+     * @param \Throwable $e
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|object|\Symfony\Component\HttpFoundation\Response|Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+        $response = parent::render($request, $e);
+
+        if (!app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
+            return Inertia::render('Error', ['title' => $this->titles[$response->status()], 'description' => $this->descriptions[$response->status()]])
+                ->toResponse($request)
+                ->setStatusCode($response->status());
+        } elseif ($response->status() === 419) {
+            return back()->with([
+                'message' => 'The page expired, please try again.',
+            ]);
+        }
+
+        return $response;
     }
 }
