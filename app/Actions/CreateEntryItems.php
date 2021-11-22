63266103c9e3a796f2b648e43dbffbaaa6dca0e1
Massimo Triassi <kidmax14@hotmail.com>
2021-11-22 17:27:21 -0500

    feat: #183 add entry item action

---
+++ app/Actions/CreateEntryItems.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Entry;
+use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateEntryItems
+{
+    use AsAction;
+
+    /**
+     * Expect an array of item details, create each item and attach them to the appropriate relation objects
+     *
+     * @param Entry $entry
+     * @param array $items
+     */
+    public function handle(Entry $entry, array $items)
+    {
+        foreach ($items as $item) {
+            Item::create(
+                array_merge([
+                    'author_id' => Auth::id(),
+                    'entry_id' => $entry->id,
+                    'character_id' => $entry->character_id
+                ], $item)
+            );
+        }
+    }
+}
