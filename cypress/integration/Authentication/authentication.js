0c31d0f8d038f390d527af509a85d2c0b2be9205
Pascal <pascal.demerdjian@gmail.com>
2021-11-28 18:00:56 -0500

    test: #179 add typos in password input to check that feedback is given to the user

--- cypress/integration/Authentication/authentication.js
+++ cypress/integration/Authentication/authentication.js
@@ -2,6 +2,7 @@ describe('Character Exploration Test Suite', () => {
     const testusername = 'testuser'
     const testuser_email = 'testuser@email.com'
     const testuser_password = 'testpass'
+    const incomplete_password = testuser_password.substr(0, testuser_password.length - 1)
 
     before(() => {
         cy.refreshDatabase()
@@ -17,7 +18,10 @@ describe('Character Exploration Test Suite', () => {
         cy.get('#username').type(testusername)
         cy.get('#email').type(testuser_email)
         cy.get('#password').type(testuser_password)
-        cy.get('#confirmPassword').type(testuser_password)
+        cy.get('#confirmPassword').type(incomplete_password)
+        cy.get('button[type="submit"]').click()
+        cy.contains('The password confirmation does not match.')
+        cy.get('#confirmPassword').clear().type(testuser_password)
         cy.get('button[type="submit"]').click()
         cy.wait('@register')
         cy.get('[data-cy="user"]').should('contain', testusername)
@@ -31,7 +35,10 @@ describe('Character Exploration Test Suite', () => {
         cy.get('[data-testid="PersonIcon"]').click()
         cy.contains('button', 'Login').click()
         cy.get('#email').type(testuser_email)
-        cy.get('#password').type(testuser_password)
+        cy.get('#password').type(incomplete_password)
+        cy.get('button[type="submit"]').click()
+        cy.contains('These credentials do not match our records')
+        cy.get('#password').clear().type(testuser_password)
         cy.get('button[type="submit"]').click()
         cy.wait('@login')
         cy.get('[data-cy="user"]').should('contain', testusername)
