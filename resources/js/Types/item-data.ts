85004dea0d83a6912091aeae0ad1b27ab319eefe
luigibesani <luigi.besani@gmail.com>
2021-12-08 00:01:15 -0500

    chore: #124 add item-data type

---
+++ resources/js/Types/item-data.ts
@@ -0,0 +1,12 @@
+export type ItemData = {
+    id: number
+    entry_id: number
+    character_id: number
+    name: string
+    author_id: string
+    rarity: string
+    tier: string
+    description: string
+    created_at: string
+    updated_at: string
+}
