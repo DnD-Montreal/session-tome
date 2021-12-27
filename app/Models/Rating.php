e5d931c5319ac3b1de5c611ac4e4479b6660856f
IanjhPhillips <ianjhphillips@gmail.com>
2021-12-27 15:57:17 -0500

    feat: #205 add bitmasking to rating model

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -19,6 +19,7 @@ class Rating extends Model
         'user_id',
         'author_id',
         'score',
+        'categories',
     ];
 
     /**
@@ -31,7 +32,14 @@ class Rating extends Model
         'entry_id' => 'integer',
         'user_id' => 'integer',
         'author_id' => 'integer',
+        'categories' => 'integer',
     ];
+
+    public const CREATIVE_BITMASK = 1 << 0;    // 00001
+    public const FLEXIBLE_BITMASK = 1 << 1;    // 00010
+    public const FRIENDLY_BITMASK = 1 << 2;    // 00100
+    public const HELPFUL_BITMASK = 1 << 3;     // 01000
+    public const PREPARED_BITMASK = 1 << 4;    // 10000
 
 
     public function entry()
@@ -47,5 +55,34 @@ class Rating extends Model
     public function author()
     {
         return $this->belongsTo(\App\Models\User::class);
+    }
+
+    /**
+     * Determines if the rating instance has a category given by the passed parameter
+     * @param $bitMask
+     * bitmask param should always be one of the bitmask constants defined on this model
+     * @return bool
+     */
+    public function hasCategory($bitMask)
+    {
+        return ($this->categories & $bitMask) === $bitMask;
+    }
+
+    /**
+     * Adds a category given by the bitmask parameter, which should be one of the bitmask constants defined on this model
+     * @param $bitMask
+     */
+    public function addCategory($bitMask)
+    {
+        $this->categories |= $bitMask;
+    }
+
+    /**
+     * Removes a category given by the bitmask parameter, which should be one of the bitmask constants defined on this model
+     * @param $bitMask
+     */
+    public function removeCategory($bitMask)
+    {
+        $this->categories &= ~$bitMask;
     }
 }
