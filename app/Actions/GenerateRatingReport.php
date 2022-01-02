b37639b0f18acf81456914edf811a6426fe4f3d5
Massimo Triassi <kidmax14@hotmail.com>
2022-01-02 12:52:16 -0500

    chore: #208 add action scaffold

---
+++ app/Actions/GenerateRatingReport.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class GenerateRatingReport
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
