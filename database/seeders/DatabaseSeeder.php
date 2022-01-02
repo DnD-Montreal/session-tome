2868918814b0a0ac70f2616cfa7f98ea293ac4ca
Massimo Triassi <kidmax14@hotmail.com>
2022-01-02 13:17:46 -0500

    chore: #208 add rating + event generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Character;
 use App\Models\Entry;
+use App\Models\Event;
 use App\Models\Item;
+use App\Models\Rating;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +20,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $users = User::factory(10)->create();
-        $dm = User::factory()->create();
+        $dm = User::factory(5)->create();
+        $events = Event::factory(5)->create();
         $characters = [];
 
         foreach ($users as $user) {
@@ -31,7 +34,9 @@ class DatabaseSeeder extends Seeder
                 'user_id' => $user->id,
                 'type' => Entry::TYPE_DM,
                 'levels' => rand(0, 1),
-                'character_id' => null
+                'event_id' => $events->random()->id,
+                'character_id' => null,
+                'dungeon_master_id' => null
             ]);
         }
 
@@ -40,7 +45,7 @@ class DatabaseSeeder extends Seeder
                 'character_id' => $character->id,
                 'user_id' => $character->user->id,
                 'levels' => random_int(0, 1),
-                'dungeon_master_id' => $dm->id
+                'dungeon_master_id' => $dm->random()->id
             ]);
         }
 
@@ -49,6 +54,12 @@ class DatabaseSeeder extends Seeder
                 Item::factory(rand(0, 1))->create([
                     'entry_id' => $entry->id,
                     'character_id' => $entry->character_id,
+                    'author_id' => $entry->user_id
+                ]);
+            } elseif ($entry->dungeon_master_id) {
+                Rating::factory()->create([
+                    'entry_id' => $entry->id,
+                    'user_id' => $entry->dungeon_master_id,
                     'author_id' => $entry->user_id
                 ]);
             }
