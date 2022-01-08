b0820b3a792eed775d7baff590183a1aa393ea3b
Massimo Triassi <kidmax14@hotmail.com>
2022-01-08 18:57:41 -0500

    fix: #204 address security hotspot, http used instead of https

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -109,7 +109,7 @@ return [
     'developer_name' => 'Cristian Tabacitu',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://tabacitu.ro',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => true,
