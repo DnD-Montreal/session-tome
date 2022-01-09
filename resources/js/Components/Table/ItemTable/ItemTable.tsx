43da20066b070232c49414bdf927594c3216f5cd
luigibesani <luigi.besani@gmail.com>
2022-01-08 19:01:39 -0500

    refactor: #193 refactor ItemTable to make use of <DataTable/> component

--- resources/js/Components/Table/ItemTable/ItemTable.tsx
+++ resources/js/Components/Table/ItemTable/ItemTable.tsx
@@ -1,235 +1,141 @@
+import {useForm} from '@inertiajs/inertia-react'
+import AddIcon from '@mui/icons-material/Add'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import FileDownloadIcon from '@mui/icons-material/FileDownload'
-import {
-    Stack,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TablePagination,
-    TableRow,
-} from '@mui/material'
-import Box from '@mui/material/Box'
-import Checkbox from '@mui/material/Checkbox'
-import Chip from '@mui/material/Chip'
-import IconButton from '@mui/material/IconButton'
-import {alpha} from '@mui/material/styles'
-import Toolbar from '@mui/material/Toolbar'
-import Tooltip from '@mui/material/Tooltip'
-import Typography from '@mui/material/Typography'
-import {RarityChip} from 'Components'
-import {RowData} from 'Mock/item-data'
+import PublishIcon from '@mui/icons-material/Publish'
+import {Box, Button, IconButton, Stack, Tooltip} from '@mui/material'
+import {DataTable, DeleteModal, Link, RarityChip} from 'Components'
 import React, {useState} from 'react'
-
-const EnhancedTableToolbar = ({numSelected}: {numSelected: number}) => (
-    <Toolbar
-        sx={{
-            pl: {sm: 2},
-            pr: {xs: 1, sm: 1},
-            ...(numSelected > 0 && {
-                bgcolor: (theme) =>
-                    alpha(
-                        theme.palette.primary.main,
-                        theme.palette.action.activatedOpacity,
-                    ),
-            }),
-        }}>
-        {numSelected > 0 ? (
-            <Typography
-                sx={{flex: '1 1 100%'}}
-                color='inherit'
-                variant='subtitle1'
-                component='div'>
-                {numSelected} selected
-            </Typography>
-        ) : (
-            <Typography
-                sx={{flex: '1 1 100%', fontFamily: 'Cinzel Decorative'}}
-                variant='h6'
-                id='tableTitle'
-                component='div'>
-                Items
-            </Typography>
-        )}
-        {numSelected > 0 ? (
-            <Stack direction='row'>
-                <Tooltip title='Export'>
-                    <IconButton>
-                        <FileDownloadIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title='Delete'>
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            </Stack>
-        ) : null}
-    </Toolbar>
-)
+import {ItemData} from 'Types/item-data'
+import route from 'ziggy-js'
 
 type ItemTablePropType = {
-    rows: RowData[]
+    data: ItemData[]
+    setIsEditDrawerOpen: (payload: boolean) => void
+    setEditId: (payload: number) => void
+    setEditData: (payload: ItemData) => void
 }
 
-const ItemTable = ({rows}: ItemTablePropType) => {
-    const [selected, setSelected] = useState<readonly string[]>([])
-    const [page, setPage] = useState(0)
-    const [rowsPerPage, setRowsPerPage] = useState(5)
+type FormDataType = {
+    items: number[]
+}
 
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage)
-    }
+const ItemTable = ({
+    data,
+    setIsEditDrawerOpen,
+    setEditId,
+    setEditData,
+}: ItemTablePropType) => {
+    const [selected, setSelected] = useState<number[]>([])
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false)
+    const {setData, delete: destroy} = useForm<FormDataType>({items: []})
 
-    const handleChangeRowsPerPage = (event: any) => {
-        setRowsPerPage(parseInt(event.target.value, 10))
-        setPage(0)
-    }
+    const leftActions = [
+        <Link href='./'>
+            <Button variant='contained' startIcon={<AddIcon />}>
+                Create
+            </Button>
+        </Link>,
+        <Link href='./'>
+            <Button variant='contained' startIcon={<PublishIcon />}>
+                Import
+            </Button>
+        </Link>,
+        <Button variant='contained' startIcon={<FileDownloadIcon />}>
+            Export
+        </Button>,
+    ]
 
-    const handleSelectAllClick = (event: any) => {
-        if (event.target.checked) {
-            const newSelecteds = rows.map((n) => n.iname)
-            setSelected(newSelecteds)
-            return
-        }
-        setSelected([])
-    }
-
-    const handleClick = (event: any, name: string) => {
-        const selectedIndex = selected.indexOf(name)
-        let newSelected: readonly string[] = []
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name)
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1))
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1))
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            )
-        }
-        setSelected(newSelected)
-    }
-
-    const isSelected = (name: string) => selected.indexOf(name) !== -1
+    const columns = [
+        {
+            property: 'name',
+            title: 'Name',
+            render: (value: string, row: ItemData) => (
+                <Link href={route('item.show', [row.id])}>{value}</Link>
+            ),
+        },
+        {
+            property: 'description',
+            title: 'Description',
+        },
+        {
+            property: 'rarity',
+            title: 'Rarity',
+            render: (value: string) => <RarityChip value={value} />,
+        },
+        {
+            property: 'tier',
+            title: 'Tier',
+        },
+        {
+            property: null,
+            title: 'Actions',
+            render: (row: any) => (
+                <>
+                    <IconButton
+                        onClick={() => {
+                            setEditData(row)
+                            setEditId(row.id)
+                            setIsEditDrawerOpen(true)
+                        }}
+                        aria-label='edit'>
+                        <EditIcon />
+                    </IconButton>
+                    <IconButton aria-label='delete'>
+                        <DeleteIcon
+                            onClick={() => {
+                                setData('items', [row.id])
+                                setIsDeleteModalOpen(true)
+                            }}
+                        />
+                    </IconButton>
+                </>
+            ),
+        },
+    ]
+    const bulkSelectActions = selected.length > 0 && (
+        <Stack direction='row' justifyContent='flex-end'>
+            <Tooltip title='Delete'>
+                <IconButton>
+                    <DeleteIcon
+                        onClick={() => {
+                            setData('items', selected)
+                            setIsDeleteModalOpen(true)
+                        }}
+                    />
+                </IconButton>
+            </Tooltip>
+        </Stack>
+    )
 
     return (
         <Box>
-            <EnhancedTableToolbar numSelected={selected.length} />
-            <TableContainer>
-                <Table sx={{minWidth: 650}} aria-label='simple table'>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell padding='checkbox'>
-                                <Checkbox
-                                    data-testid='header-checkbox'
-                                    id='header-checkbox'
-                                    color='primary'
-                                    indeterminate={
-                                        selected.length > 0 &&
-                                        selected.length < rows.length
-                                    }
-                                    checked={
-                                        rows.length > 0 && selected.length === rows.length
-                                    }
-                                    onChange={handleSelectAllClick}
-                                    inputProps={{
-                                        'aria-label': 'select all names',
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell padding='none' align='left'>
-                                Name
-                            </TableCell>
-                            <TableCell align='center'>Rarity</TableCell>
-                            <TableCell align='center'>Tier</TableCell>
-                            <TableCell align='center'>Description</TableCell>
-                            <TableCell align='center'>Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row: RowData, index: number) => {
-                                const isItemSelected = isSelected(row.iname)
-                                const labelId = `enhanced-table-checkbox-${index}`
-                                return (
-                                    <TableRow
-                                        key={row.iname}
-                                        sx={{
-                                            '&:last-child td, &:last-child th': {
-                                                border: 0,
-                                            },
-                                        }}
-                                        hover
-                                        onClick={(event) => handleClick(event, row.iname)}
-                                        role='checkbox'
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        selected={isItemSelected}>
-                                        <TableCell padding='checkbox'>
-                                            <Checkbox
-                                                color='primary'
-                                                checked={isItemSelected}
-                                                inputProps={{
-                                                    'aria-labelledby': labelId,
-                                                }}
-                                                id={`checkbox-${index}`}
-                                                data-testid={`checkbox-${index}`}
-                                            />
-                                        </TableCell>
-                                        <TableCell
-                                            component='th'
-                                            id={labelId}
-                                            scope='row'
-                                            padding='none'>
-                                            {row.iname}
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            <RarityChip value={row.rarity} />
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            <Chip
-                                                label={row.tier}
-                                                color='default'
-                                                variant='outlined'
-                                            />
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            {row.description}
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            <IconButton aria-label='edit'>
-                                                <EditIcon />
-                                            </IconButton>
-                                            <IconButton aria-label='delete'>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component='div'
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+            <DeleteModal
+                open={isDeleteModalOpen}
+                warningMessage='Are you sure you want to delete this/these item(s)?'
+                onClose={() => setIsDeleteModalOpen(false)}
+                onDelete={() => {
+                    destroy(route('item.destroy'))
+                    if (selected) {
+                        setSelected([])
+                    }
+                }}
+            />
+            <DataTable
+                leftActions={leftActions}
+                selected={selected}
+                setSelected={setSelected}
+                isSelectable
+                data={data}
+                columns={columns}
+                tableName='Items'
+                bulkSelectActions={bulkSelectActions}
+                filterProperties={['name']}
             />
         </Box>
     )
 }
 
-EnhancedTableToolbar.displayName = 'EnhancedTableToolbar'
 ItemTable.displayName = 'ItemTable'
 export default ItemTable
