fdb1475b2240cce586d3b3d557c1930e6f95f904
Roberto Nittolo <nitrobert@yahoo.com>
2022-01-08 20:09:03 -0500

    fix: #204  remove ability for a user to be created

--- app/Http/Controllers/Admin/UserCrudController.php
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -29,6 +29,7 @@ class UserCrudController extends CrudController
         CRUD::setModel(\App\Models\User::class);
         CRUD::setRoute(config('backpack.base.route_prefix') . '/user');
         CRUD::setEntityNameStrings('user', 'users');
+        $this->crud->denyAccess('create');
     }
 
     /**
