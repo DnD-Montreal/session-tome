58f330d7b0168af50b5d6acd34816c02e275e789
luigibesani <luigi.besani@gmail.com>
2022-01-09 01:53:32 -0500

    chore: #124 update Item page, ItemTable, and CharacterItemCreate form tests

---
+++ resources/js/Components/Form/CharacterItemCreateForm.test.js
@@ -0,0 +1,56 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import React from 'react'
+
+import CharacterItemCreateForm from './CharacterItemCreateForm'
+
+const mockFunction = jest.fn()
+const editProps = {
+    type: 'Edit',
+    onCloserDrawer: mockFunction,
+    editData: {
+        name: '',
+        description: '',
+        rarity: '',
+        tier: 1,
+    },
+    editId: 0,
+}
+
+const createProps = {
+    type: 'Create',
+}
+
+describe('<CharacterItemCreateForm />', () => {
+    it('edit component should render', () => {
+        const component = render(<CharacterItemCreateForm {...editProps} />)
+        expect(component).toBeDefined()
+    })
+    it('create component should render', () => {
+        const component = render(<CharacterItemCreateForm {...createProps} />)
+        expect(component).toBeDefined()
+    })
+    it('create component fields test', () => {
+        render(<CharacterItemCreateForm {...createProps} />)
+        const nameField = document.querySelector('#name')
+        const descriptionField = document.querySelector('#description')
+        const rarityField = document.querySelector('#rarity')
+        const tierField = document.querySelector('#tier')
+        fireEvent.change(nameField, {target: {value: '123'}})
+        fireEvent.change(descriptionField, {target: {value: '123'}})
+        fireEvent.change(rarityField, {target: {value: '123'}})
+        fireEvent.change(tierField, {target: {value: 2}})
+        fireEvent.click(screen.getByText('Create'))
+    })
+    it('edit component fields test', () => {
+        render(<CharacterItemCreateForm {...editProps} />)
+        const nameField = document.querySelector('#name')
+        const descriptionField = document.querySelector('#description')
+        const rarityField = document.querySelector('#rarity')
+        const tierField = document.querySelector('#tier')
+        fireEvent.change(nameField, {target: {value: '123'}})
+        fireEvent.change(descriptionField, {target: {value: '123'}})
+        fireEvent.change(rarityField, {target: {value: '123'}})
+        fireEvent.change(tierField, {target: {value: 2}})
+        fireEvent.click(screen.getByText('Save'))
+    })
+})
--- resources/js/Components/Table/ItemTable/ItemTable.test.js
+++ resources/js/Components/Table/ItemTable/ItemTable.test.js
@@ -8,7 +8,7 @@ const mockFunction = jest.fn()
 const isSelectedtest = (name) => [].indexOf(name) !== -1
 const props = {
     isSelected: isSelectedtest,
-    rows: itemData,
+    data: itemData,
     selected: [],
     handleClick: mockFunction,
     handleSelectAllClick: mockFunction,
@@ -18,31 +18,13 @@ describe('ItemTable', () => {
         const component = render(<ItemTable {...props} />)
         expect(component).toBeDefined()
     })
-
-    it('shoud render all checkboxes for each data row', () => {
-        render(<ItemTable {...props} />)
-        const checkbox = screen.getAllByRole('checkbox')
-        expect(checkbox).toHaveLength(itemData.length)
-        fireEvent.click(screen.getByLabelText('select all names'))
-        for (let i = 0; i < 5; i += 1) {
-            const checkboxel = screen
-                .getByTestId(`checkbox-${i}`)
-                .querySelector('input[type="checkbox"]')
-            expect(checkboxel).toHaveProperty('checked', true)
-        }
-        const checkbox1 = screen
-            .getByTestId('checkbox-1')
-            .querySelector('input[type="checkbox"]')
-        fireEvent.click(screen.getByTestId('checkbox-1'))
-        expect(checkbox1).toHaveProperty('checked', false)
-    })
     it('Pagination handleChangePage should work', () => {
         render(<ItemTable {...props} />)
         fireEvent.click(screen.getByTitle('Go to next page'))
     })
     it('Pagination 5 click should work', () => {
         render(<ItemTable {...props} />)
-        fireEvent.click(screen.getByLabelText('5'))
-        fireEvent.click(screen.getByDisplayValue('5'))
+        fireEvent.click(screen.getByLabelText('10'))
+        fireEvent.click(screen.getByDisplayValue('10'))
     })
 })
