475a3f22b487de20c6441e4d5220423029c8a017
Roberto Nittolo <nitrobert@yahoo.com>
2022-01-08 19:01:03 -0500

    feat: #204 replace icons in backpack sidbar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,14 +1,14 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('adventure') }}'><i class='nav-icon la la-question'></i> Adventures</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class='nav-icon la la-question'></i> Campaigns</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('character') }}'><i class='nav-icon la la-question'></i> Characters</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('entry') }}'><i class='nav-icon la la-question'></i> Entries</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class='nav-icon la la-question'></i> Events</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class='nav-icon la la-question'></i> Items</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('league') }}'><i class='nav-icon la la-question'></i> Leagues</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('rating') }}'><i class='nav-icon la la-question'></i> Ratings</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('role') }}'><i class='nav-icon la la-question'></i> Roles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('session') }}'><i class='nav-icon la la-question'></i> Sessions</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('trade') }}'><i class='nav-icon la la-question'></i> Trades</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('adventure') }}'><i class="las la-dice-d20"></i> Adventures</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class="las la-dice-d20"></i> Campaigns</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('character') }}'><i class="las la-dice-d20"></i> Characters</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('entry') }}'><i class="las la-dice-d20"></i> Entries</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class="las la-dice-d20"></i> Events</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class="las la-dice-d20"></i> Items</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('league') }}'><i class="las la-dice-d20"></i> Leagues</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('rating') }}'><i class="las la-dice-d20"></i> Ratings</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('role') }}'><i class="las la-dice-d20"></i> Roles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('session') }}'><i class="las la-dice-d20"></i> Sessions</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('trade') }}'><i class="las la-dice-d20"></i> Trades</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class="las la-dice-d20"></i> Users</a></li>
