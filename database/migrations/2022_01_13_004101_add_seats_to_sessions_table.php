59f1ac2e89b25c326b0ef4d3f7c62dc7462a8508
Massimo Triassi <kidmax14@hotmail.com>
2022-01-12 20:40:16 -0500

    fix: #250 add missing forign key constraint disables to down methods

--- database/migrations/2022_01_13_004101_add_seats_to_sessions_table.php
+++ database/migrations/2022_01_13_004101_add_seats_to_sessions_table.php
@@ -14,7 +14,7 @@ class AddSeatsToSessionsTable extends Migration
     public function up()
     {
         Schema::table('sessions', function (Blueprint $table) {
-            $table->integer('seats')->after('table');
+            $table->integer('seats')->nullable()->after('table');
         });
     }
 
