33540f5318f821a6223b1cd508c6164042b649f0
luigibesani <luigi.besani@gmail.com>
2022-01-16 16:58:21 -0500

    chore: #263 convert getUser to arrow function + move fct into react module

--- resources/js/Components/CharacterDetail/CharacterDetailBox.tsx
+++ resources/js/Components/CharacterDetail/CharacterDetailBox.tsx
@@ -31,83 +31,86 @@ type CharDetailBoxPropType = {
     setIsEditDrawerOpen: (payload: boolean) => void
 }
 
-function getUser() {
-    const {auth} = usePage<UsePageType>().props
-    const {user} = auth
-    return user?.id
-}
+const CharacterDetailBox = ({character, setIsEditDrawerOpen}: CharDetailBoxPropType) => {
+    const getUser = () => {
+        const {auth} = usePage<UsePageType>().props
+        const {user} = auth
+        return user?.id
+    }
 
-const CharacterDetailBox = ({character, setIsEditDrawerOpen}: CharDetailBoxPropType) => (
-    <Box sx={{p: 5, backgroundColor: 'primary'}}>
-        <Grid container spacing={6}>
-            <Grid item xs={4}>
-                <Img
-                    alt='avatar'
-                    src='https://static.wikia.nocookie.net/eladriells-dd/images/d/d8/Orc.jpg/revision/latest/scale-to-width-down/600?cb=20190330101155'
-                />
+    return (
+        <Box sx={{p: 5, backgroundColor: 'primary'}}>
+            <Grid container spacing={6}>
+                <Grid item xs={4}>
+                    <Img
+                        alt='avatar'
+                        src='https://static.wikia.nocookie.net/eladriells-dd/images/d/d8/Orc.jpg/revision/latest/scale-to-width-down/600?cb=20190330101155'
+                    />
+                </Grid>
+                <Grid item container columnSpacing={1} rowSpacing={5} xs={8}>
+                    <Grid item xs={12}>
+                        <StyledTypography>NAME</StyledTypography>
+                        <Typography>{character.name}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>RACE</StyledTypography>
+                        <Typography>{character.race}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>CLASS</StyledTypography>
+                        <Typography>{character.class}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>FACTION</StyledTypography>
+                        <Typography>{character.faction}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>LEVEL</StyledTypography>
+                        <Typography>{character.level}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>DOWNTIME</StyledTypography>
+                        <Typography>{character.downtime}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTypography>STATUS</StyledTypography>
+                        <StyledStatus>{character.status}</StyledStatus>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Stack spacing={3} direction='row'>
+                            {getUser() === character.user_id && (
+                                <Button
+                                    data-testid='update-button'
+                                    variant='contained'
+                                    startIcon={<CreateIcon fontSize='small' />}
+                                    onClick={() => {
+                                        setIsEditDrawerOpen(true)
+                                    }}>
+                                    UPDATE
+                                </Button>
+                            )}
+                            {getUser() === character.user_id && (
+                                <Button
+                                    variant='contained'
+                                    startIcon={<DownloadIcon fontSize='small' />}>
+                                    EXPORT
+                                </Button>
+                            )}
+                            <Link
+                                href={route('item.index', {character_id: character.id})}>
+                                <Button
+                                    variant='contained'
+                                    startIcon={<IosShareIcon fontSize='small' />}>
+                                    ITEMS
+                                </Button>
+                            </Link>
+                        </Stack>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid item container columnSpacing={1} rowSpacing={5} xs={8}>
-                <Grid item xs={12}>
-                    <StyledTypography>NAME</StyledTypography>
-                    <Typography>{character.name}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>RACE</StyledTypography>
-                    <Typography>{character.race}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>CLASS</StyledTypography>
-                    <Typography>{character.class}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>FACTION</StyledTypography>
-                    <Typography>{character.faction}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>LEVEL</StyledTypography>
-                    <Typography>{character.level}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>DOWNTIME</StyledTypography>
-                    <Typography>{character.downtime}</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <StyledTypography>STATUS</StyledTypography>
-                    <StyledStatus>{character.status}</StyledStatus>
-                </Grid>
-                <Grid item xs={12}>
-                    <Stack spacing={3} direction='row'>
-                        {getUser() === character.user_id && (
-                            <Button
-                                data-testid='update-button'
-                                variant='contained'
-                                startIcon={<CreateIcon fontSize='small' />}
-                                onClick={() => {
-                                    setIsEditDrawerOpen(true)
-                                }}>
-                                UPDATE
-                            </Button>
-                        )}
-                        {getUser() === character.user_id && (
-                            <Button
-                                variant='contained'
-                                startIcon={<DownloadIcon fontSize='small' />}>
-                                EXPORT
-                            </Button>
-                        )}
-                        <Link href={route('item.index', {character_id: character.id})}>
-                            <Button
-                                variant='contained'
-                                startIcon={<IosShareIcon fontSize='small' />}>
-                                ITEMS
-                            </Button>
-                        </Link>
-                    </Stack>
-                </Grid>
-            </Grid>
-        </Grid>
-    </Box>
-)
+        </Box>
+    )
+}
 
 CharacterDetailBox.displayName = 'CharacterDetailBox'
 export default CharacterDetailBox
