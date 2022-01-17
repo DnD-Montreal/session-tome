21a2b17d94ea19ee14f59964e07569ee8f0e951f
Roberto Nittolo <nitrobert@yahoo.com>
2022-01-17 17:54:08 -0500

    feat: #240 add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('auth:sanctum')
+    ->name('login');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::post('/character', [App\Http\Controllers\CharacterController::class, 'create'])
+    ->middleware('auth:sanctum')
+    ->name("character.create");
+
+Route::delete('/character/{character?}', [App\Http\Controllers\CharacterController::class, 'destroy'])
+    ->middleware('auth:sanctum')
+    ->name("character.destroy");
