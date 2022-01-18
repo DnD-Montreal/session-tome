0dedcf1ba56d6b7813c71f392eb6115c92ebf74d
IanjhPhillips <ianjhphillips@gmail.com>
2022-01-18 18:50:45 -0500

    refactor: #249 rename button

--- resources/views/vendor/backpack/crud/buttons/entry_report.blade.php
+++ resources/views/vendor/backpack/crud/buttons/entry_report.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ url($crud->route.'/'.$entry->getKey().'/report') }} " class="btn btn-xs btn-default"><i class="la la-bar-chart-o"></i> Moderate</a>
+<a href="{{ url($crud->route.'/'.$entry->getKey().'/report') }} " class="btn btn-xs btn-default"><i class="la la-bar-chart-o"></i> Generate Report</a>
