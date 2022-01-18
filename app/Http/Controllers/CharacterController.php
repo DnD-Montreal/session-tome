47c1150884ef21c557e7cc9d67d899f7305dfb64
Massimo Triassi <kidmax14@hotmail.com>
2022-01-18 18:31:36 -0500

    feat: #181 add sort by query param for index

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -17,8 +17,19 @@ class CharacterController extends Controller
      */
     public function index(Request $request)
     {
-        $characters = Character::where('user_id', Auth::user()->id)->get();
+        $characters = Character::where('user_id', Auth::user()->id);
         $factions = array_values(Character::FACTIONS);
+        $sortParams = $request->validate([
+            'sort_by' => "sometimes|in:name,race,class,level,faction,downtime",
+            'sort_dir' => "sometimes|in:asc,desc"
+        ]);
+
+        if (isset($sortParams['sort_by'])) {
+            $direction =  isset($sortParams['sort_dir']) ? $sortParams['sort_dir'] : "asc";
+            $characters = $characters->orderBy($sortParams['sort_by'], $direction);
+        }
+
+        $characters = $characters->get();
 
         return Inertia::render('Character/Character', compact('characters', 'factions'));
     }
