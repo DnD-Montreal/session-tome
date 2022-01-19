42c055ab035455ba6f2ba4d8b695dd93f1475cbc
Annie Tran <annietran13@hotmail.com>
2022-01-19 18:12:15 -0500

    refactor: #215 remove empty tag

--- resources/js/Components/Form/RatingForm.tsx
+++ resources/js/Components/Form/RatingForm.tsx
@@ -36,89 +36,85 @@ const StyledButton = styled(Button)`
 `
 
 const RatingForm = ({ratings, setData}: RatingFormPropType) => (
-    <>
-        <StyledBox>
-            <StyledTypography>
-                Select what you like about your Game Master.
-            </StyledTypography>
-            <Stack spacing={2} direction='row'>
-                <StyledButton
-                    onClick={() => {
-                        setData('rating_data', {...ratings, creative: !ratings.creative})
-                    }}
-                    variant={ratings.creative ? 'contained' : 'outlined'}
-                    startIcon={
-                        ratings.creative ? (
-                            <BrushIcon fontSize='small' />
-                        ) : (
-                            <BrushOutlinedIcon fontSize='small' />
-                        )
-                    }
-                    className={ratings.creative ? 'filled' : ''}
-                    aria-label='creative'>
-                    Creative
-                </StyledButton>
-                <StyledButton
-                    onClick={() => {
-                        setData('rating_data', {...ratings, flexible: !ratings.flexible})
-                    }}
-                    variant={ratings.flexible ? 'contained' : 'outlined'}
-                    startIcon={<CableIcon fontSize='small' />}
-                    className={ratings.flexible ? 'filled' : ''}
-                    aria-label='flexible'>
-                    Flexible
-                </StyledButton>
-                <StyledButton
-                    onClick={() => {
-                        setData('rating_data', {...ratings, friendly: !ratings.friendly})
-                    }}
-                    variant={ratings.friendly ? 'contained' : 'outlined'}
-                    startIcon={
-                        ratings.friendly ? (
-                            <EmojiEmotionsIcon fontSize='small' />
-                        ) : (
-                            <EmojiEmotionsOutlinedIcon fontSize='small' />
-                        )
-                    }
-                    className={ratings.friendly ? 'filled' : ''}
-                    aria-label='friendly'>
-                    Friendly
-                </StyledButton>
-                <StyledButton
-                    onClick={() => {
-                        setData('rating_data', {...ratings, helpful: !ratings.helpful})
-                    }}
-                    variant={ratings.helpful ? 'contained' : 'outlined'}
-                    startIcon={
-                        ratings.helpful ? (
-                            <EventAvailableIcon fontSize='small' />
-                        ) : (
-                            <EventAvailableOutlinedIcon fontSize='small' />
-                        )
-                    }
-                    className={ratings.helpful ? 'filled' : ''}
-                    aria-label='helpful'>
-                    Helpful
-                </StyledButton>
-                <StyledButton
-                    onClick={() => {
-                        setData('rating_data', {...ratings, prepared: !ratings.prepared})
-                    }}
-                    variant={ratings.prepared ? 'contained' : 'outlined'}
-                    startIcon={
-                        ratings.prepared ? (
-                            <TimerIcon fontSize='small' />
-                        ) : (
-                            <TimerOutlinedIcon fontSize='small' />
-                        )
-                    }
-                    className={ratings.prepared ? 'filled' : ''}
-                    aria-label='prepared'>
-                    Prepared
-                </StyledButton>
-            </Stack>
-        </StyledBox>
-    </>
+    <StyledBox>
+        <StyledTypography>Select what you like about your Game Master.</StyledTypography>
+        <Stack spacing={2} direction='row'>
+            <StyledButton
+                onClick={() => {
+                    setData('rating_data', {...ratings, creative: !ratings.creative})
+                }}
+                variant={ratings.creative ? 'contained' : 'outlined'}
+                startIcon={
+                    ratings.creative ? (
+                        <BrushIcon fontSize='small' />
+                    ) : (
+                        <BrushOutlinedIcon fontSize='small' />
+                    )
+                }
+                className={ratings.creative ? 'filled' : ''}
+                aria-label='creative'>
+                Creative
+            </StyledButton>
+            <StyledButton
+                onClick={() => {
+                    setData('rating_data', {...ratings, flexible: !ratings.flexible})
+                }}
+                variant={ratings.flexible ? 'contained' : 'outlined'}
+                startIcon={<CableIcon fontSize='small' />}
+                className={ratings.flexible ? 'filled' : ''}
+                aria-label='flexible'>
+                Flexible
+            </StyledButton>
+            <StyledButton
+                onClick={() => {
+                    setData('rating_data', {...ratings, friendly: !ratings.friendly})
+                }}
+                variant={ratings.friendly ? 'contained' : 'outlined'}
+                startIcon={
+                    ratings.friendly ? (
+                        <EmojiEmotionsIcon fontSize='small' />
+                    ) : (
+                        <EmojiEmotionsOutlinedIcon fontSize='small' />
+                    )
+                }
+                className={ratings.friendly ? 'filled' : ''}
+                aria-label='friendly'>
+                Friendly
+            </StyledButton>
+            <StyledButton
+                onClick={() => {
+                    setData('rating_data', {...ratings, helpful: !ratings.helpful})
+                }}
+                variant={ratings.helpful ? 'contained' : 'outlined'}
+                startIcon={
+                    ratings.helpful ? (
+                        <EventAvailableIcon fontSize='small' />
+                    ) : (
+                        <EventAvailableOutlinedIcon fontSize='small' />
+                    )
+                }
+                className={ratings.helpful ? 'filled' : ''}
+                aria-label='helpful'>
+                Helpful
+            </StyledButton>
+            <StyledButton
+                onClick={() => {
+                    setData('rating_data', {...ratings, prepared: !ratings.prepared})
+                }}
+                variant={ratings.prepared ? 'contained' : 'outlined'}
+                startIcon={
+                    ratings.prepared ? (
+                        <TimerIcon fontSize='small' />
+                    ) : (
+                        <TimerOutlinedIcon fontSize='small' />
+                    )
+                }
+                className={ratings.prepared ? 'filled' : ''}
+                aria-label='prepared'>
+                Prepared
+            </StyledButton>
+        </Stack>
+    </StyledBox>
 )
 
 RatingForm.displayName = 'RatingForm'
