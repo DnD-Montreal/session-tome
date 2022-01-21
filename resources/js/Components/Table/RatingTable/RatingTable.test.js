8c1205175c3849cb802673362432890821f85a38
wilson <wh.dev@icloud.com>
2022-01-21 18:22:30 -0500

    fix: unit test

--- resources/js/Components/Table/RatingTable/RatingTable.test.js
+++ resources/js/Components/Table/RatingTable/RatingTable.test.js
@@ -4,13 +4,9 @@ import React from 'react'
 
 import RatingTable from './RatingTable'
 
-const mockFunction = jest.fn()
 const props = {
     reportedRatings: ratingData,
-    isFiltered: false,
-    setIsFiltered: mockFunction,
 }
-const chipIconSelector = 'svg[data-testid="DoneIcon"]'
 
 describe('CharacterTable', () => {
     it('Component should render', () => {
@@ -20,17 +16,5 @@ describe('CharacterTable', () => {
     it('Pagination handleChangePage should work', () => {
         render(<RatingTable {...props} />)
         fireEvent.click(screen.getByTitle('Go to next page'))
-    })
-    it('Filter Chip Deactivated', () => {
-        render(<RatingTable {...props} />)
-        const filterChip = screen.getByText('League Event Ratings Only')
-        expect(document.querySelector(chipIconSelector)).toBeNull()
-        fireEvent.click(filterChip)
-        expect(mockFunction).toBeCalled()
-    })
-    it('Filter Chip Activated', () => {
-        props.isFiltered = true
-        render(<RatingTable {...props} />)
-        expect(document.querySelector(chipIconSelector))
     })
 })
