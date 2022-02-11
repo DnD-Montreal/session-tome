c43e8d78bb75f4f2a498d5752f1192d770d002d2
Annie Tran <annietran13@hotmail.com>
2022-02-11 18:09:11 -0500

    fix: #343 reward choice shows on edit form

--- resources/js/Components/Form/DmEntryCreateForm.tsx
+++ resources/js/Components/Form/DmEntryCreateForm.tsx
@@ -98,7 +98,7 @@ const DmEntryCreateForm = ({
                   gp: editData?.gp || 0,
                   location: editData?.location || '',
                   date_played: editData?.date_played || new Date().toDateString(),
-                  choice: '',
+                  choice: editData?.reward || '',
                   character_id: editData?.character_id || null,
                   notes: editData?.notes || '',
                   items: editData?.items || [],
