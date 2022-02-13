920cfb08de3d6d285eac8fc75653b2543b92e22c
bcobo341 <60267926+bcobo341@users.noreply.github.com>
2022-02-13 18:03:34 -0500

    fix: #376 use one search parameter for Filterable trait
    Co-authored-by: Massimo <m-triassi@users.noreply.github.com>

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -18,8 +18,7 @@ class TradeController extends Controller
     {
         $trades = Trade::where('status', 'open')
                     ->with('item')
-                    ->filtered($request->get('requested_items'))
-                    ->filtered($request->get('description'));
+                    ->filtered($request->get('search'))
 
         if ($itemName = $request->get('item_name')) {
             $trades = $trades->whereHas('item', function (Builder $q) use ($itemName) {
