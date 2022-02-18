9193ddeb9aaea529af9b89d00dfe09218de595e0
wilson <wh.dev@icloud.com>
2022-02-18 00:08:59 -0500

    chore: address design changes

--- resources/js/Pages/Profile.tsx
+++ resources/js/Pages/Profile.tsx
@@ -11,61 +11,80 @@ const Profile = () => {
 
     return (
         <ThemeProvider theme={theme}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <Typography>Email</Typography>
-                    <TextField
-                        fullWidth
-                        placeholder='Email'
-                        size='small'
-                        value={user.email}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography>Username</Typography>
-                    <TextField
-                        fullWidth
-                        placeholder='Username'
-                        size='small'
-                        value={user.name}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography>Change Password</Typography>
-                    <TextField
-                        fullWidth
-                        placeholder='New password'
-                        size='small'
-                        style={{marginBottom: 6}}
-                    />
-                    <TextField
-                        fullWidth
-                        placeholder='Confirm new password'
-                        size='small'
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography>Language</Typography>
-                    <Select size='small' options={['English', 'French']} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant='contained' size='small'>
-                        Save
-                    </Button>
-                </Grid>
-                <Grid item xs={12}>
-                    <Divider style={{width: '100%'}} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography color='error'>Delete User Account</Typography>
-                    <Typography>
-                        Warning! This will permanently delete your account
-                    </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant='contained' size='small' color='error'>
-                        Delete my account
-                    </Button>
+            <Grid container rowSpacing={2}>
+                <Grid item container xs={6}>
+                    <Grid item xs={12}>
+                        <Typography variant='h4'>Account Settings</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider style={{width: '100%'}} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='h5'>Email</Typography>
+                        <TextField
+                            fullWidth
+                            placeholder='Email'
+                            size='small'
+                            value={user.email}
+                        />
+                    </Grid>
+                    <Grid item xs={12} />
+                    <Grid item xs={12}>
+                        <Typography variant='h5'>Username</Typography>
+                        <TextField
+                            fullWidth
+                            placeholder='Username'
+                            size='small'
+                            value={user.name}
+                        />
+                    </Grid>
+                    <Grid item xs={12} />
+                    <Grid item xs={12}>
+                        <Typography variant='h5'>Change Password</Typography>
+                        <TextField
+                            fullWidth
+                            placeholder='New password'
+                            size='small'
+                            style={{marginBottom: 6}}
+                            type='password'
+                        />
+                        <TextField
+                            fullWidth
+                            placeholder='Confirm new password'
+                            size='small'
+                            type='password'
+                        />
+                    </Grid>
+                    <Grid item xs={12} />
+                    <Grid item xs={12}>
+                        <Typography variant='h5'>Language</Typography>
+                        <Select size='small' options={['English', 'French']} />
+                    </Grid>
+                    <Grid item xs={12} />
+                    <Grid container item xs={12}>
+                        <Button size='small' color='secondary' style={{marginLeft: 6}}>
+                            Reset
+                        </Button>
+                        <Button variant='contained' size='small' style={{marginLeft: 6}}>
+                            Save
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider style={{width: '100%'}} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='h5' color='error'>
+                            Delete User Account
+                        </Typography>
+                        <Typography>
+                            Warning! This will permanently delete your account.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant='contained' size='small' color='error'>
+                            Delete my account
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
         </ThemeProvider>
