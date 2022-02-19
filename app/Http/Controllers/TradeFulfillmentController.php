9bb378c3877d7d46a66bdae71862957cd2c37908
IanjhPhillips <ianjhphillips@gmail.com>
2022-02-19 17:54:55 -0500

    feat: #378 create trade fulfill controller

--- app/Http/Controllers/TradeFulfillmentController.php
+++ app/Http/Controllers/TradeFulfillmentController.php
@@ -1,7 +1,30 @@
 <?php
 
-namespace App\Http;
+namespace App\Http\Controllers;
 
-class TradeFulfillmentController
+use App\Exceptions\TradeException;
+use App\Http\Requests\TradeFulfillRequest;
+use App\Actions\FulfillTrade;
+use App\Models\Item;
+use App\Models\Trade;
+use Inertia\Inertia;
+
+class TradeFulfillmentController extends Controller
 {
+    /**
+     * @param App\Http\Requests\TradeFulfillRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(TradeFulfillRequest $request, Trade $trade)
+    {
+        $offeredItem = Item::findOrFail($request->get('offered_item_id'));
+
+        try {
+            FulfillTrade::run($trade, $offeredItem);
+        } catch (TradeException $e) {
+            return redirect()->back()->withException($e);
+        }
+
+        return redirect()->back();
+    }
 }
