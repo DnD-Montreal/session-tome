f427b68aa0269da4f2671b9ca2c05376980112a5
IanjhPhillips <ianjhphillips@gmail.com>
2022-02-19 17:51:18 -0500

    feat: #378 create trade fulfill request class

--- app/Http/Requests/TradeFulfillRequest.php
+++ app/Http/Requests/TradeFulfillRequest.php
@@ -2,6 +2,30 @@
 
 namespace App\Http\Requests;
 
-class TradeFulfillRequest
+use Illuminate\Foundation\Http\FormRequest;
+
+class TradeFulfillRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('update', $this->trade);
+    }
 }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'character_id' => ['required', 'integer', 'exists:characters,id'],
+            'offered_item_id' => ['required', 'integer', 'exists:items,id'],
+        ];
+    }
+
