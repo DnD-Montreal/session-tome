157b6f855057ee7d8adacc355c5b0a0e62820e64
Annie Tran <annietran13@hotmail.com>
2022-02-18 21:07:40 -0500

    chore: #387 remove unused import and commented out code

--- resources/js/Pages/Campaign/Campaign.tsx
+++ resources/js/Pages/Campaign/Campaign.tsx
@@ -4,17 +4,10 @@ import {ThemeProvider} from '@mui/material/styles'
 import {Link} from 'Components'
 import {ApplicationLayout} from 'Layouts'
 import React from 'react'
-// import {CampaignData} from 'Types/campaign-data'
-// import {CharacterData} from 'Types/character-data'
 import {getFontTheme} from 'Utils'
 import route from 'ziggy-js'
 
 const theme = getFontTheme('Form', 16)
-
-type CampaignPropType = {
-    // campaign: CampaignData
-    // characters: CharacterData[]
-}
 
 const Campaign = () => (
     <ThemeProvider theme={theme}>
