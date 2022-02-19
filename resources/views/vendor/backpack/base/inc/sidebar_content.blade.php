0c9c77ea6e747a475d2bbac62738b197755c612d
Roberto Nittolo <nitrobert@yahoo.com>
2022-02-19 01:07:33 -0500

    feat: #374 restrict access to sidebar based on role type

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,14 +1,22 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-dice-d20 nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('adventure') }}'><i class="las la-dragon"></i> Adventures</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class="las la-user-friends"></i> Campaigns</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('character') }}'><i class="las la-user-shield"></i> Characters</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('entry') }}'><i class="las la-pen-fancy"></i> Entries</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class="las la-calendar"></i> Events</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class="las la-dice-d20"></i> Items</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('league') }}'><i class="las la-users"></i> Leagues</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('rating') }}'><i class="las la-chart-bar"></i> Ratings</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('role') }}'><i class="las la-user-tag"></i> Roles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('session') }}'><i class="las la-chair"></i> Sessions</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('trade') }}'><i class="las la-exchange-alt"></i> Trades</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class="las la-user"></i> Users</a></li>
+@if (Auth::user()->isSiteAdmin())
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-dice-d20 nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('adventure') }}'><i class="las la-dragon"></i> Adventures</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class="las la-user-friends"></i> Campaigns</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('character') }}'><i class="las la-user-shield"></i> Characters</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('entry') }}'><i class="las la-pen-fancy"></i> Entries</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class="las la-calendar"></i> Events</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class="las la-dice-d20"></i> Items</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('league') }}'><i class="las la-users"></i> Leagues</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('rating') }}'><i class="las la-chart-bar"></i> Ratings</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('role') }}'><i class="las la-user-tag"></i> Roles</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('session') }}'><i class="las la-chair"></i> Sessions</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('trade') }}'><i class="las la-exchange-alt"></i> Trades</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class="las la-user"></i> Users</a></li>
+@elseif (Auth::user()->isLeagueAdminRole())
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('event') }}'><i class="las la-calendar"></i> Events</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('session') }}'><i class="las la-chair"></i> Sessions</a></li>
+@endif
+
+
+
