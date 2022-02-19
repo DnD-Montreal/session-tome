a5188412ad939329a19ef3b4e9f96e0544db8b39
IanjhPhillips <ianjhphillips@gmail.com>
2022-02-19 17:51:50 -0500

    feat: #378 create trade update policy

--- app/Policies/TradePolicy.php
+++ app/Policies/TradePolicy.php
@@ -2,6 +2,26 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+use App\Models\Trade;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Http\Request;
+
 class TradePolicy
 {
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
 }
+    public function update(User $user, Trade $trade)
+    {
+        return $trade->character->user->id == $user->id || $user->isSiteAdmin();
+    }
