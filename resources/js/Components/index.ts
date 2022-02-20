66aadd96541d3d66144bf32a1f8e648cb8eade88
Annie Tran <annietran13@hotmail.com>
2022-02-19 20:29:59 -0500

    feat: #387 campaign join modal

--- resources/js/Components/index.ts
+++ resources/js/Components/index.ts
@@ -1,5 +1,6 @@
 // Components
 export {default as Authentication} from './Authentication'
+export {default as CampaignJoinModal} from './CampaignJoinModal'
 export {default as DeleteModal} from './DeleteModal'
 export {default as ErrorText} from './ErrorText'
 
