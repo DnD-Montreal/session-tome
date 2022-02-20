d0adbb4b77f89c4118460cda3af7ab12bdaeff3a
luigibesani <luigi.besani@gmail.com>
2022-02-20 10:42:20 -0500

    test: #384 add index test and table test

---
+++ resources/js/Components/Table/CampaignTable/CampaignTable.test.js
@@ -0,0 +1,35 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import {campaignData} from 'Mock/campaign-data'
+import React from 'react'
+
+import CampaignTable from './CampaignTable'
+
+const mockFunction = jest.fn()
+const props = {
+    data: campaignData,
+    setEditData: mockFunction,
+    setEditId: mockFunction,
+    setIsEditDrawerOpen: mockFunction,
+}
+
+describe('CampaignTable', () => {
+    it('Component should render', () => {
+        const component = render(<CampaignTable {...props} />)
+        expect(component).toBeDefined()
+    })
+    it('Pagination handleChangePage should work', () => {
+        render(<CampaignTable {...props} />)
+        fireEvent.click(screen.getByTitle('Go to next page'))
+    })
+    it('Pagination 5 click should work', () => {
+        render(<CampaignTable {...props} />)
+        fireEvent.click(screen.getByLabelText('10'))
+        fireEvent.click(screen.getByDisplayValue('10'))
+    })
+    it('Edit Icon', () => {
+        render(<CampaignTable {...props} />)
+        const editIcon = document.querySelector('[data-testid="EditIcon"]')
+        fireEvent.click(editIcon)
+        expect(mockFunction).toBeCalled()
+    })
+})
