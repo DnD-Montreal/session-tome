65d84ddc86131ef25614749af67e0c52d095acba
IanjhPhillips <ianjhphillips@gmail.com>
2022-02-19 19:58:50 -0500

    fix: #378 fix method param comment block

--- app/Http/Controllers/TradeFulfillmentController.php
+++ app/Http/Controllers/TradeFulfillmentController.php
@@ -7,13 +7,14 @@ use App\Http\Requests\TradeFulfillRequest;
 use App\Actions\FulfillTrade;
 use App\Models\Item;
 use App\Models\Trade;
+use Illuminate\Http\RedirectResponse;
 
 class TradeFulfillmentController extends Controller
 {
     /**
-     * @param App\Http\Requests\TradeFulfillRequest $request
-     * @param App\Http\Models\Trade $trade
-     * @return \Illuminate\Http\RedirectResponse
+     * @param TradeFulfillRequest $request
+     * @param Trade $trade
+     * @return RedirectResponse
      */
     public function store(TradeFulfillRequest $request, Trade $trade)
     {
