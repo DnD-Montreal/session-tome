799c58c209621c2b5ea4807daa5dbe82c13232d3
luigibesani <luigi.besani@gmail.com>
2022-02-21 19:54:34 -0500

    fix: #425 swap out location column for description

--- resources/js/Components/Table/EventTable/EventTable.tsx
+++ resources/js/Components/Table/EventTable/EventTable.tsx
@@ -3,46 +3,46 @@ import React from 'react'
 import {EventData} from 'Types/event-data'
 
 type EventTablePropType = {
-  data: EventData[]
+    data: EventData[]
 }
 
 type FormDataType = {
-  events: number[]
+    events: number[]
 }
 
 const EventTable = ({data}: EventTablePropType) => {
-  const columns = [
-    {
-      property: 'created_at',
-      title: 'Date',
-    },
-    {
-      property: 'title',
-      title: 'Event Title',
-    },
-    {
-      property: 'league_id',
-      title: 'League',
-    },
-    {
-      property: 'location',
-      title: 'Location',
-    },
-    {
-      property: 'participation',
-      title: 'Participation',
-    },
-  ]
+    const columns = [
+        {
+            property: 'created_at',
+            title: 'Date',
+        },
+        {
+            property: 'title',
+            title: 'Event Title',
+        },
+        {
+            property: 'league_id',
+            title: 'League',
+        },
+        {
+            property: 'description',
+            title: 'Description',
+        },
+        {
+            property: 'participation',
+            title: 'Participation',
+        },
+    ]
 
-  return (
-    <DataTable
-      isSelectable={false}
-      data={data}
-      columns={columns}
-      tableName='Events'
-      filterProperties={['title', 'location']}
-    />
-  )
+    return (
+        <DataTable
+            isSelectable={false}
+            data={data}
+            columns={columns}
+            tableName='Events'
+            filterProperties={['title', 'location']}
+        />
+    )
 }
 
 EventTable.displayName = 'EventTable'
