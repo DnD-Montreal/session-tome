737b080741b3cacfcba4ff3e5b6973a17ebbf8c0
Roberto Nittolo <nitrobert@yahoo.com>
2022-02-21 21:29:42 -0500

    test: #374 add tests for site admins and league admins

--- tests/Feature/BackPackControllerTest.php
+++ tests/Feature/BackPackControllerTest.php
@@ -31,18 +31,28 @@ class BackPackControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+    }
+
+    public function createLeagueAdmin()
+    {
+        $this->user = User::factory()->create();
+        $siteRole = Role::create(['name' => "League Admin", 'type' => Role::LEAGUE_ADMIN]);
+        $this->user->roles()->attach($siteRole);
+        Auth::login($this->user);
+    }
+
+    public function createSiteAdmin()
+    {
         $this->user = User::factory()->create();
         $siteRole = Role::create(['name' => "test site admin", 'type' => Role::SITE_ADMIN]);
         $this->user->roles()->attach($siteRole);
         Auth::login($this->user);
     }
 
-    public function createLeagueAdmin()
+    public function createNonAdmin()
     {
-        $leagueAdmin = User::factory()->create();
-        $siteRole = Role::create(['name' => "League Admin", 'type' => Role::LEAGUE_ADMIN]);
-        $leagueAdmin->roles()->attach($siteRole);
-        Auth::login($leagueAdmin);
+        $this->user = User::factory()->create();
+        Auth::login($this->user);
     }
 
     /**
@@ -50,6 +60,7 @@ class BackPackControllerTest extends TestCase
      */
     public function test_admin_user_has_access()
     {
+        $this->createSiteAdmin();
         $response = $this->get(('/admin/dashboard'));
         $response->assertOk();
     }
@@ -59,10 +70,8 @@ class BackPackControllerTest extends TestCase
      */
     public function test_league_admin_user_has_access()
     {
-        $leagueAdmin = User::factory()->create();
-        $siteRole = Role::create(['name' => "League Admin", 'type' => Role::LEAGUE_ADMIN]);
-        $leagueAdmin->roles()->attach($siteRole);
-        $response = $this->actingAs($leagueAdmin)->get(('/admin/dashboard'));
+        $this->createLeagueAdmin();
+        $response = $this->get(('/admin/dashboard'));
         $response->assertOk();
     }
 
@@ -71,8 +80,8 @@ class BackPackControllerTest extends TestCase
      */
     public function test_user_does_not_have_access()
     {
-        $nonAdmin = User::factory()->create();
-        $response = $this->actingAs($nonAdmin)->get(('/admin/dashboard'));
+        $this->createNonAdmin();
+        $response = $this->get(('/admin/dashboard'));
         $this->assertEquals(302, $response->status());
     }
 
@@ -81,6 +90,8 @@ class BackPackControllerTest extends TestCase
      */
     public function test_backpack_crud_actions()
     {
+        $this->createSiteAdmin();
+
         $models = ['adventure', 'campaign', 'character', 'entry', 'event', 'league', 'rating', 'role', 'session', 'trade', 'user'];
         $classes = [Adventure::class, Campaign::class, Character::class, Entry::class, Event::class, League::class, Rating::class, Role::class, Session::class, Trade::class, User::class];
 
@@ -144,5 +155,73 @@ class BackPackControllerTest extends TestCase
             $response = $this->get("/admin/$models[$i]/$id/show");
             $response->assertOk();
         }
+    }
+
+    /**
+     * @test
+     */
+    public function test_backpack_not_authorized_for_league_admin_session()
+    {
+        $this->createLeagueAdmin();
+
+        $event = Event::factory()->create();
+        $adventure = Adventure::factory()->create();
+        $dungeon_master = User::factory()->create();
+        $table = $this->faker->word;
+        $start_time = $this->faker->dateTime();
+
+        $response = $this->post('/admin/session', [
+            'event_id' => $event->id,
+            'adventure_id' => $adventure->id,
+            'dungeon_master_id' => $dungeon_master->id,
+            'table' => $table,
+            'start_time' => $start_time,
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * @test
+     */
+    public function test_backpack_not_authorized_for_league_admin_event()
+    {
+        $this->createLeagueAdmin();
+
+        $league = League::factory()->create();
+        $title = $this->faker->sentence(4);
+        $description = $this->faker->text;
+        $location = $this->faker->word;
+
+        $response = $this->post('/admin/event', [
+            'league_id' => $league->id,
+            'title' => $title,
+            'description' => $description,
+            'location' => $location,
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * @test
+     */
+    public function test_backpack_authorized_for_site_admin_event()
+    {
+        $this->createSiteAdmin();
+
+        $league = League::factory()->create();
+        $title = $this->faker->sentence(4);
+        $description = $this->faker->text;
+        $location = $this->faker->word;
+
+        $response = $this->post('/admin/event', [
+            'league_id' => $league->id,
+            'title' => $title,
+            'description' => $description,
+            'location' => $location,
+        ]);
+
+        $response->assertRedirect();
     }
 }
