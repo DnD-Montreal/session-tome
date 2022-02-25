149ddfd9b68dbefd07c1940467c667801f0ee0b8
Annie Tran <annietran13@hotmail.com>
2022-02-25 18:11:06 -0500

    chore: #417 usercharacterdata type change

--- resources/js/Types/user-character-data.ts
+++ resources/js/Types/user-character-data.ts
@@ -1,36 +1,7 @@
-import {CharacterData} from 'Types/character-data'
-import {ItemData} from 'Types/item-data'
-import {RatingCategoryType} from 'Types/rating-data'
+import {EntriesData} from 'Types/entries-data'
 
 export type UserCharacterData = {
     id: number
     name: string
     entries: EntriesData[]
-}
-
-export type EntriesData = {
-    adventure: any
-    adventure_id: number | null
-    campaign_id: number | null
-    character_id: number | null
-    character: CharacterData | null
-    created_at: string
-    date_played: string
-    downtime: number | null
-    length: number | null
-    dungeon_master: string | null
-    dungeon_master_id: number | null
-    event_id: number | null
-    gp: number | null
-    id: number
-    items: ItemData[] | null
-    levels: number | null
-    location: string | null
-    notes: string | null
-    reward: string | null
-    session: number | null
-    type: 'game' | 'downtime' | 'dm' | string
-    updated_at: string
-    user_id: number
-    rating_data: RatingCategoryType | null
 }
