fa2476af0084cac2395e9ae33fa4dceb54d08932
Massimo Triassi <kidmax14@hotmail.com>
2022-03-01 16:07:53 -0500

    test: #436 add tests for new registration scope/accessor

--- tests/Unit/Models/EventTest.php
+++ tests/Unit/Models/EventTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Character;
 use App\Models\Entry;
 use App\Models\Event;
 use App\Models\League;
 use App\Models\Session;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use JMac\Testing\Traits\AdditionalAssertions;
 use Tests\TestCase;
 
@@ -45,8 +48,50 @@ class EventTest extends TestCase
      */
     public function can_belong_to_league()
     {
-        $event = Event::factory(1)->create()[0];
+        $event = Event::factory()->create();
 
         $this->assertCount(1, $event->league()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function can_be_filtered_by_registered_user()
+    {
+        $characterFactory = Character::factory()->has(User::factory());
+        $sessionFactory = Session::factory()->has($characterFactory);
+        $event = Event::factory()->has($sessionFactory)->create();
+        // Create events that _shouldn't_ be returned
+        Event::factory(3)->has(Session::factory(5))->create();
+        $user = $event->sessions[0]->characters[0]->user;
+
+        $filtered = Event::whereRegistered($user->id)->get();
+        Auth::login($user);
+        $filteredViaLogin = Event::whereRegistered()->get();
+
+        $this->assertEquals($event->fresh(), $filtered[0]);
+        $this->assertEquals($filtered[0], $filteredViaLogin[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function is_registered_shows_when_authenticated_user_is_registered()
+    {
+        $characterFactory = Character::factory()->has(User::factory());
+        $sessionFactory = Session::factory()->has($characterFactory);
+        $event = Event::factory()->has($sessionFactory)->create();
+        // Create events that _shouldn't_ be returned
+        $otherEvents = Event::factory(3)->has(Session::factory(5))->create();
+        $user = $event->sessions[0]->characters[0]->user;
+        Auth::login($user);
+
+        $registeredEvent = $event->is_registered;
+        $nonRegisteredEvents = $otherEvents->map(fn ($e) => $e->is_registered);
+
+        $this->assertTrue($registeredEvent);
+        foreach ($nonRegisteredEvents as $registeredBool) {
+            $this->assertFalse($registeredBool);
+        }
     }
 }
