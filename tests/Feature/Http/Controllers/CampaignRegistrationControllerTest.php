a0ec8ccedd4b16903863242ac3e65af7d8473b40
Roberto Nittolo <nitrobert@yahoo.com>
2022-03-05 04:21:40 -0500

    test: #432 add tests for both campaign owner and non campaign owner

--- tests/Feature/Http/Controllers/CampaignRegistrationControllerTest.php
+++ tests/Feature/Http/Controllers/CampaignRegistrationControllerTest.php
@@ -106,4 +106,81 @@ class CampaignRegistrationControllerTest extends TestCase
         $this->assertDatabaseCount('campaign_user', 0);
         $this->assertDatabaseCount('campaign_character', 0);
     }
+
+    /**
+     * @test
+     */
+    public function delete_user_and_character_from_a_campaign_as_a_campaign_owner()
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $character = Character::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $character2 = Character::factory()->create([
+            'user_id' => $user2->id
+        ]);
+
+        $campaign = Campaign::factory()->create();
+
+        $this->user->campaigns()->attach($campaign, ['is_dm' => false, 'is_owner' => true]);
+        $user->campaigns()->attach($campaign, ['is_dm' => true, 'is_owner' => false]);
+        $user2->campaigns()->attach($campaign, ['is_dm' => false, 'is_owner' => false]);
+
+        $campaign->characters()->attach($character);
+        $campaign->characters()->attach($character2);
+
+
+        $response = $this->delete(route('campaign-registration.destroy', $campaign), [
+                'user_id' => [$character->user_id, $character2->user_id]
+        ]);
+
+
+
+        $response->assertRedirect();
+        $this->assertDatabaseCount('campaign_user', 1);
+        $this->assertDatabaseCount('campaign_character', 0);
+        $this->assertDatabaseHas('campaign_user', ['user_id' => $this->user->id, 'campaign_id' => $campaign->id, 'is_dm' => false, 'is_owner' => true]);
+        $this->assertNotEquals($campaign->code, Campaign::first()->code);
+    }
+
+    /**
+     * @test
+     */
+    public function delete_user_and_character_from_a_campaign_without_being_a_campaign_owner()
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $character = Character::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $character2 = Character::factory()->create([
+            'user_id' => $user2->id
+        ]);
+
+        $campaign = Campaign::factory()->create();
+
+        $this->user->campaigns()->attach($campaign, ['is_dm' => false, 'is_owner' => false]);
+        $user->campaigns()->attach($campaign, ['is_dm' => false, 'is_owner' => true]);
+        $user2->campaigns()->attach($campaign, ['is_dm' => true, 'is_owner' => false]);
+
+        $campaign->characters()->attach($character);
+        $campaign->characters()->attach($character2);
+
+        $response = $this->delete(route('campaign-registration.destroy', $campaign), [
+            'user_id' => [$character->user_id, $character2->user_id]
+        ]);
+
+        $response->assertRedirect();
+        $this->assertDatabaseCount('campaign_user', 3);
+        $this->assertDatabaseCount('campaign_character', 2);
+        $this->assertDatabaseHas('campaign_user', ['user_id' => $user->id, 'campaign_id' => $campaign->id, 'is_dm' => false, 'is_owner' => true]);
+        $this->assertDatabaseHas('campaign_user', ['user_id' => $user2->id, 'campaign_id' => $campaign->id, 'is_dm' => true, 'is_owner' => false]);
+        $this->assertDatabaseHas('campaign_user', ['user_id' => $this->user->id, 'campaign_id' => $campaign->id, 'is_dm' => false, 'is_owner' => false]);
+        $this->assertEquals($campaign->code, Campaign::first()->code);
+    }
 }
