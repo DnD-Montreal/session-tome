ae148b722944bdd1d414c8b50fe79f1db6dd2d2c
Massimo Triassi <kidmax14@hotmail.com>
2022-03-19 21:25:53 -0400

    refactor: #499 reduce duplication by extracting steps that happen under both conditions of update

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -132,15 +132,16 @@ class CampaignController extends Controller
             ->where('user_id', "!=", Auth::id())
             ->pluck('id');
 
-        if (!empty($data['character_id'])) {
+        if ($hasCharacter = !empty($data['character_id'])) {
             // Add the new character ID to be attached, along with the rest of the characters.
             $campaign->characters()->sync($playerCharacters->prepend($data['character_id']));
-            $campaign->users()->updateExistingPivot(Auth::user(), ['is_dm' => 0]);
         } else {
             // If we dont have a character_id then the user is becoming the DM...
             $campaign->characters()->sync($playerCharacters);
-            $campaign->users()->updateExistingPivot(Auth::user(), ['is_dm' => 1]);
         }
+
+        // If the user doesnt have a character, then they're a DM, otherwise, they're not
+        $campaign->users()->updateExistingPivot(Auth::user(), ['is_dm' => !$hasCharacter]);
 
         $campaign->update($data);
 
