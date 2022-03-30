cdeecbf050096a806b091d151d7ffc32c9a3f730
Roberto Nittolo <nitrobert@yahoo.com>
2022-03-30 03:45:30 -0400

    refactor: #447  address code smells, address duplication

--- tests/Feature/Http/Controllers/LocustAuthControllerTest.php
+++ tests/Feature/Http/Controllers/LocustAuthControllerTest.php
@@ -11,19 +11,27 @@ class LocustAuthControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * @test
-     */
-    public function test_get_token()
+    public string $bearer = 'Bearer ';
+    public $token;
+
+    public function setUp(): void
     {
-        $token = config('app.key');
+        parent::setUp();
+        $this->token = config('app.key');
         app()->detectEnvironment(function () {
             return 'load';
         });
 
         //Initial request to make the locust user from the Authenticate method in the Authenticate middleware
-        $this->withHeaders(['Authorization' => 'Bearer '.$token])->get(route('character.index'));
-        $response = $this->withHeaders(['Authorization' => 'Bearer '.$token])->get('api/locust');
+        $this->withHeaders(['Authorization' => $this->bearer .$this->token])->get(route('character.index'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_get_token()
+    {
+        $response = $this->withHeaders(['Authorization' => $this->bearer .$this->token])->get('api/locust');
 
         $response->assertStatus(200);
     }
@@ -33,20 +41,12 @@ class LocustAuthControllerTest extends TestCase
      */
     public function test_delete_character()
     {
-        $token = config('app.key');
-        app()->detectEnvironment(function () {
-            return 'load';
-        });
-
-        //Initial request to make the locust user from the Authenticate method in the Authenticate middleware
-        $this->withHeaders(['Authorization' => 'Bearer '.$token])->get(route('character.index'));
-
         $user = User::first();
         Character::factory(5)->create([
             'user_id' => $user->id
         ]);
 
-        $response = $this->withHeaders(['Authorization' => 'Bearer '.$token])->delete('api/character');
+        $response = $this->withHeaders(['Authorization' => $this->bearer .$this->token])->delete('api/character');
 
         $response->assertStatus(200);
         $this->assertDatabaseCount('characters', 4);
@@ -57,21 +57,13 @@ class LocustAuthControllerTest extends TestCase
      */
     public function test_cleanUp()
     {
-        $token = config('app.key');
-        app()->detectEnvironment(function () {
-            return 'load';
-        });
-
-        //Initial request to make the locust user from the Authenticate method in the Authenticate middleware
-        $this->withHeaders(['Authorization' => 'Bearer '.$token])->get(route('character.index'));
-
         $user = User::first();
         Character::factory(5)->create([
             'user_id' => $user->id
         ]);
 
-        $this->withHeaders(['Authorization' => 'Bearer '.$token])->get('api/locust');
-        $response = $this->withHeaders(['Authorization' => 'Bearer '.$token])->delete('api/clean');
+        $this->withHeaders(['Authorization' => $this->bearer .$this->token])->get('api/locust');
+        $response = $this->withHeaders(['Authorization' => $this->bearer .$this->token])->delete('api/clean');
 
         $response->assertStatus(200);
         $this->assertDatabaseCount('characters', 0);
