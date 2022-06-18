0749994b8c79686179880d40512cc77657b387f4
wilson <wh.dev@icloud.com>
2022-06-18 13:57:16 -0400

    feat: improve welcome page

--- resources/js/Pages/Welcome/Welcome.tsx
+++ resources/js/Pages/Welcome/Welcome.tsx
@@ -1,19 +1,46 @@
-import {Box, Container, Grid, Typography} from '@mui/material'
-import {Select} from 'Components'
+import {Grid, Link, Typography} from '@mui/material'
 import i18n from 'i18next'
+import dndBanner from 'Images/dnd-banner.jpg'
 import {useTranslation} from 'react-i18next'
 import styled from 'styled-components'
 import {UserType} from 'Types/global'
 
-const StyledBox = styled(Box)`
+const Container = styled.div`
+    text-align: center;
+    max-width: 70%;
+    margin: 0px auto;
+
+    @media only screen and (max-width: 1200px) {
+        width: 100%;
+    }
+`
+
+const StyledImg = styled.img`
+    @media only screen and (max-width: 1200px) {
+        width: 65vw;
+    }
+    max-height: 40vh;
+`
+
+const StyledText = styled.p`
     font-family: 'Cinzel Decorative', cursive;
     font-size: 16px;
+    display: inline;
     text-align: center;
 `
 
-const CenteredGrid = styled(Grid)`
-    align-items: center;
-    justify-content: center;
+const StyledTypography = styled(Typography)<{align?: string}>`
+    text-align: ${(props) => (props.align ? props.align : 'start')};
+    margin-bottom: 12px;
+`
+
+const StyledLink = styled(Link)`
+    display: inline;
+    text-decoration: none;
+`
+
+const LanguageSelector = styled(Typography)`
+    cursor: pointer;
 `
 
 type WelcomeProps = {
@@ -28,36 +55,57 @@ const Welcome = (props: WelcomeProps) => {
     const {user} = auth
 
     return (
-        <Container maxWidth='lg'>
-            <CenteredGrid
-                container
-                spacing={{xs: 2, md: 6}}
-                direction='column'
-                alignItems='center'
-                justifyContent='center'>
+        <Container>
+            <Grid container spacing={4}>
                 <Grid item xs={12}>
-                    <Typography sx={{textAlign: 'center'}} component='div'>
+                    <StyledTypography align='center'>
                         {t('welcome.welcome-text-1')}
-                        <StyledBox display='inline'>{t('welcome.session-tome')}</StyledBox>
+                        <StyledText>{t('welcome.session-tome')}</StyledText>
+                    </StyledTypography>
+                    <StyledTypography align='center'>
                         {t('welcome.welcome-text-2')}
-                    </Typography>
+                    </StyledTypography>
                 </Grid>
-            </CenteredGrid>
-            {!user && (
-                <Grid container style={{marginTop: 16}}>
-                    <Grid item xs={4} />
-                    <Grid item xs={4}>
-                        <Select
-                            options={['English', 'Français']}
-                            onChange={(e) => {
-                                i18n.changeLanguage(e.target.value === 'English' ? 'en' : 'fr')
-                            }}
-                            defaultValue='English'
-                        />
+                <Grid item xs={12}>
+                    <StyledImg src={dndBanner} alt='dungeon-and-dragons-banner' />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                    <StyledTypography variant='h4'>{t('welcome.what-is-dnd')}</StyledTypography>
+                    <StyledTypography>{t('welcome.dnd-definition')}</StyledTypography>
+                    <StyledTypography>
+                        {t('welcome.more-definition')}{' '}
+                        <StyledLink href='https://dnd.wizards.com/what-is-dnd' target='_blank'>
+                            official Wizards of the Coast website.
+                        </StyledLink>
+                    </StyledTypography>
+                </Grid>
+                <Grid item md={6} xs={12}>
+                    <StyledTypography variant='h4'>
+                        {t('welcome.what-is-adventure-league')}
+                    </StyledTypography>
+                    <StyledTypography>{t('welcome.adventure-league-definition')}</StyledTypography>
+                    <StyledTypography>
+                        {t('welcome.more-definition')}{' '}
+                        <StyledLink
+                            href='http://dndadventurersleague.org/start-here/'
+                            target='_blank'>
+                            official website.
+                        </StyledLink>
+                    </StyledTypography>
+                </Grid>
+                {!user && (
+                    <Grid item xs={12}>
+                        <Container style={{width: '20%'}}>
+                            <LanguageSelector onClick={() => i18n.changeLanguage('en')}>
+                                English
+                            </LanguageSelector>
+                            <LanguageSelector onClick={() => i18n.changeLanguage('fr')}>
+                                Français
+                            </LanguageSelector>
+                        </Container>
                     </Grid>
-                    <Grid item xs={4} />
-                </Grid>
-            )}
+                )}
+            </Grid>
         </Container>
     )
 }
